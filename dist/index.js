c802d79dc11a03272067228590d646956f36cca7
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2023-12-04 08:47:02 +0100

    final commit

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,13 @@
 "use strict";
-console.log('hello');
+const panels = document.querySelectorAll('.panel');
+panels.forEach(panel => {
+    panel.addEventListener('click', () => {
+        removeActiveClasses();
+        panel.classList.add('active');
+    });
+});
+function removeActiveClasses() {
+    panels.forEach(panel => {
+        panel.classList.remove('active');
+    });
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log('hello');
+const panels: NodeListOf<HTMLDivElement> = document.querySelectorAll('.panel');
+
+panels.forEach( panel => {
+    panel.addEventListener('click', () => {
+        removeActiveClasses()
+        panel.classList.add('active')
+    })
+})
+
+function removeActiveClasses() {
+    panels.forEach(panel => {
+        panel.classList.remove('active')
+    })
+}
